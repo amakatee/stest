74b373b31685b92ebd5cf57f9bb8fe707597d00b
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-02-22 16:21:27 +0300

    k

---
+++ src/components/MainLayout.tsx
@@ -0,0 +1,17 @@
+interface Props {
+    children: React.ReactNode;
+  }
+  
+
+export default function Layout({ children } : Props) {
+    
+
+  
+
+  return (
+      <>
+      <div> { children}</div>
+      <div>navbar</div>
+
+      </>
+  ) }
