8434fd6a8740bb9dbb764e13ebc3574f84e53a76
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2023-04-17 19:58:55 +0300

    dvsd

--- src/components/elements/SinglePaymentPackage.tsx
+++ src/components/elements/SinglePaymentPackage.tsx
@@ -25,41 +25,49 @@ const SinglePaymentPackage = ({ packid, box}: Props) : ReactElement => {
     console.log(box)
 
     return (
-        <li className='box'>
+        <li className='box flex flex-col items-center'>
+            <p className='text-xs p-3 flex justify-start w-full' >
+              {format(box?.createdAt as Date, 'PPpp')}
+            </p>
         
       
-            {/* <div>
+           
+
+           <div className='flex items-center justify-between  pb-3 px-4 w-full'>
+
+
+
+               <div className='flex items-center gap-2'>
                 <input 
-                 name={packid}
-                 type="checkbox"
-                 checked={boxes?.find((box )  => box.id === packid)?.checked as boolean}
-                 onChange={(e: React.ChangeEvent<HTMLInputElement> ) =>{ if(handleCheckbox != undefined) {handleCheckbox(e, packid as string)} else { console.log('und')}}}
-                 
+                   className="relative float-left  h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ml-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ml-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent disabled:opacity-60 dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary dark:focus:before:shadow-[0px_0px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca]"
+                   name={packid}
+                   type="checkbox"
+                //  checked={boxes?.find((box )  => box.id === packid)?.checked as boolean}
+                //  onChange={(e: React.ChangeEvent<HTMLInputElement> ) =>{ if(handleCheckbox != undefined) {handleCheckbox(e, packid as string)} else { console.log('und')}}}
                 />
-            </div> */}
+               <div className='single-p-img p-3'>
+                <img className='w-[4rem] '  src='../box.png' />
+               </div>
 
-
-            <div className='single-p-img'>
-                <img  src='../box.png' />
-            </div>
-
-            <div className='single-p-m'>
-                {format(box?.createdAt as Date, 'PPpp')}
+                <div className='single-p-m'>
                 <p><span>Billing: </span>{box?.billing}</p>
                 <p><span>Type: </span>{box?.type}</p>
                 <p><span>Recipient: </span>{box?.recipient}</p>
                 <p><span>Country: </span>{box?.country}</p>
+               </div>
+
+               </div>
             
-            </div>
-           
-    
-           
-            <div className='flex flex-col'>
-               
-                <button type='button'>
-                    see detail
+            
+         
+         
+                <button type='button' className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-2 py-1 text-center inline-flex items-center dark:focus:ring-gray-500 text-xs">
+                    details
                 </button>    
-            </div>
+        
+
+           </div>
+           
         
         </li>
     )
--- src/pages/packagestatus/payment.tsx
+++ src/pages/packagestatus/payment.tsx
@@ -17,26 +17,25 @@ const Payment : NextPage = () => {
     // data?.currentUser?.packingorder?.map((pack) => console.log(pack))
     return (
         <Layout>
-            {data?.currentUser?.packingorder.length && data?.currentUser?.packingorder?.map((box) => 
-            <div key={box.id}
-            onClick={() => setPackageDetail({...packageDetail, show: !packageDetail.show, id:box.id})}>
-    
-             <SinglePaymentPackage packid={packageDetail.id} box={box} />
-             </div>)}
-
-
-            {packageDetail.show && <PackingPageDetail id={packageDetail.id} />}
-              {/* {data?.paymentPackages?.length ? data.paymentPackages?.map((pack, i) =>  
-                <SinglePackageItem key={i} localtracker={pack?.localtracker as string} status={pack.status} recipient={pack.recipient as string}  billing={pack.billing as number} type={pack.type as string} weight={pack.weight as number} usermessage={pack.usermessage as string}  />
-             ): <div> no data</div>} */}
-           <div className="payment">
+           <div className="payment flex py-4 items-center ">
                 <p> Total: 30$</p>
-                <button type="button" className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2">
+                <button type="button" className="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 ">
                     <svg className="w-4 h-4 mr-2 -ml-1 text-[#626890]" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="ethereum" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"></path></svg>
                     Pay with Ethereum
                  </button>
 
             </div>
+          <ul className="flex flex-col align-center justify-between gap-3 min-h-[20vh]  max-h-[70vh] overflow-scroll pb-[10vh]" >
+            {data?.currentUser?.packingorder.length && data?.currentUser?.packingorder?.map((box) => 
+            <div key={box.id}
+                  onClick={() => setPackageDetail({...packageDetail, show: !packageDetail.show, id:box.id})}>
+                   <SinglePaymentPackage packid={packageDetail.id} box={box} />
+             </div>)}
+            </ul>
+
+            {packageDetail.show && <PackingPageDetail id={packageDetail.id} />}
+             
+          
        </Layout>
       
     )
